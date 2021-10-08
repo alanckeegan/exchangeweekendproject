8288399f11ba852aab159d9ed2ca48022aaae33a
alanckeegan <alan.c.keegan@gmail.com>
2021-10-08 13:54:31 -0500

    first commit

--- server/index.js
+++ server/index.js
@@ -2,17 +2,37 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
 
-// localhost can have cross origin errors
-// depending on the browser you use!
-app.use(cors());
-app.use(express.json());
 
 const balances = {
   "1": 100,
   "2": 50,
   "3": 75,
 }
+
+for (let i =0; i < 3; i++) {
+  const newKey = ec.genKeyPair();
+  let key = {
+    private: newKey.getPrivate().toString(16),
+    public: newKey.getPublic().encode('hex').toString()
+    }
+  balances[key.public] = balances[(i+1).toString()]
+  delete balances[(i+1).toString()]
+  console.log(key)
+}
+
+console.log(balances)
+
+
+// localhost can have cross origin errors
+// depending on the browser you use!
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
@@ -21,10 +41,22 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {sender, recipient, amount, privateKey} = req.body;
+  if (privateKey && recipient && amount && sender){
+    let enteredKey = ec.keyFromPrivate(privateKey, "hex");
+    let msgHash = SHA256(sender + recipient + amount)
+    let signature = enteredKey.sign(msgHash.toString());
+    if(ec.keyFromPublic(sender, "hex").verify(msgHash.toString(), signature) && privateKey) {
+      balances[sender] -= amount;
+      balances[recipient] = (balances[recipient] || 0) + +amount;
+      res.send({ balance: balances[sender] });
+      console.log("Key Verified")
+    } else {
+    console.log("Incorrect Key")
+  }
+} else {
+  console.log("Must Complete All Fields")
+}
 });
 
 app.listen(port, () => {
